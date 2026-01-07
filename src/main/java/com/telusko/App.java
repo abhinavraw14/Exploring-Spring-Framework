5001e56cdb0f35ef24bbb969765bb4fc9715d85d
abhinavraw14 <abhinavrawat1401@gmail.com>
2026-01-07 12:59:02 +0530

    165-Object-Creation

--- src/main/java/com/telusko/App.java
+++ src/main/java/com/telusko/App.java
@@ -7,7 +7,10 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = (Alien) context.getBean("alien");
-        obj.code();
+        Alien obj1 = (Alien) context.getBean("alien1");
+        obj1.code();
+
+        Alien obj2 = (Alien) context.getBean("alien1");
+        obj2.code();
     }
 }
