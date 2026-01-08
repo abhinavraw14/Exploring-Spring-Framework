4dd83687fc0efce0494f3206b8fb3b27bf5b923b
abhinavraw14 <abhinavrawat1401@gmail.com>
2026-01-08 19:19:32 +0530

    Setter Injection

--- src/main/java/com/telusko/Alien.java
+++ src/main/java/com/telusko/Alien.java
@@ -2,7 +2,16 @@ package com.telusko;
 
 public class Alien {
 
-    int age;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("setter called");
+        this.age = age;
+    }
 
     public Alien(){
         System.out.println("Object Created");
